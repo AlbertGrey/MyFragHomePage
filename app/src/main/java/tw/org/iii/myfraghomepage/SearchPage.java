5a1e5ebba10c63ae991eb6f784fd9a6657869278
wei-chengni <greyni0330@gmail.com>
2018-04-22 03:11:45 +0800

    test7

---
+++ app/src/main/java/tw/org/iii/myfraghomepage/SearchPage.java
@@ -0,0 +1,47 @@
+package tw.org.iii.myfraghomepage;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+/**
+ * Created by wei-chengni on 2018/4/22.
+ */
+
+public class SearchPage extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_layout);
+
+        toolbar = findViewById(R.id.search_toolbar);
+        toolbar.inflateMenu(R.menu.search_menu);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search_real_btn).getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+        searchView.setSubmitButtonEnabled(true);
+
+        return true;
+    }
+}
